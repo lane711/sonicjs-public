2bd7a92a88682505a1c77ac220ae7248a5311e88
Lane Campbell <ldc0618@gmail.com>
2023-06-06 10:52:20 -0700

    fix title

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -25,9 +25,9 @@ export default function App({ Component, pageProps }) {
     <>
       <Head>
         {router.pathname === '/' ? (
-          <title>Protocol API Reference</title>
+          <title>SonicJs Headless CMS</title>
         ) : (
-          <title>{`${pageProps.title} - Protocol API Reference`}</title>
+          <title>{`${pageProps.title} - SonicJs Headless CMS`}</title>
         )}
         <meta name="description" content={pageProps.description} />
       </Head>
