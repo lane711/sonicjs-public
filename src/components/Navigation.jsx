5dacd9fc63e757741e850e238d4edb43a998de43
Lane <ldc0618@gmail.com>
2023-08-18 10:20:12 -0700

    fix typo

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -200,7 +200,7 @@ export const navigation = [
     title: 'Docs',
     links: [
       { title: 'Getting Started', href: '/getting-started' },
-      { title: 'Persistance', href: '/persistance' },
+      { title: 'persistence', href: '/persistence' },
       { title: 'Admin UI', href: '/admin-ui' },
       { title: 'Routing', href: '/routing' },
       { title: 'Data Access', href: '/data-access' },
