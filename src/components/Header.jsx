c14384664cff8dd25ccf889dd8a5ebe83751a695
Lane <ldc0618@gmail.com>
2023-08-23 09:57:39 -0700

    header update

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -30,7 +30,7 @@ function TopLevelNavItemNewTab({ href, children }) {
   return (
     <li>
       <Link
-      target='_blank'
+        target="_blank"
         href={href}
         className="text-sm leading-5 text-zinc-600 transition hover:text-zinc-900 dark:text-zinc-400 dark:hover:text-white"
       >
@@ -49,53 +49,83 @@ export const Header = forwardRef(function Header({ className }, ref) {
   let bgOpacityDark = useTransform(scrollY, [0, 72], [0.2, 0.8])
 
   return (
-    <motion.div
-      ref={ref}
-      className={clsx(
-        className,
-        'fixed inset-x-0 top-0 z-50 flex h-14 items-center justify-between gap-12 px-4 transition sm:px-6 lg:left-72 lg:z-30 lg:px-8 xl:left-80',
-        !isInsideMobileNavigation &&
-          'backdrop-blur-sm dark:backdrop-blur lg:left-72 xl:left-80',
-        isInsideMobileNavigation
-          ? 'bg-white dark:bg-zinc-900'
-          : 'bg-white/[var(--bg-opacity-light)] dark:bg-zinc-900/[var(--bg-opacity-dark)]'
-      )}
-      style={{
-        '--bg-opacity-light': bgOpacityLight,
-        '--bg-opacity-dark': bgOpacityDark,
-      }}
-    >
-      <div
+    <>
+      <motion.div
+        ref={ref}
         className={clsx(
-          'absolute inset-x-0 top-full h-px transition',
-          (isInsideMobileNavigation || !mobileNavIsOpen) &&
-            'bg-zinc-900/7.5 dark:bg-white/7.5'
+          className,
+          'fixed inset-x-0 top-0 z-50 flex h-14 items-center justify-between gap-12 px-4 transition sm:px-6 lg:left-72 lg:z-30 lg:px-8 xl:left-80',
+          !isInsideMobileNavigation &&
+            'backdrop-blur-sm dark:backdrop-blur lg:left-72 xl:left-80',
+          isInsideMobileNavigation
+            ? 'bg-white dark:bg-zinc-900'
+            : 'bg-white/[var(--bg-opacity-light)] dark:bg-zinc-900/[var(--bg-opacity-dark)]'
         )}
-      />
-      <Search />
-      <div className="flex items-center gap-5 lg:hidden">
-        <MobileNavigation />
-        <Link href="/" aria-label="Home">
-          <Logo className="h-6" />
-        </Link>
-      </div>
-      <div className="flex items-center gap-5">
-        <nav className="hidden md:block">
-          <ul role="list" className="flex items-center gap-8">
-            <TopLevelNavItem href="/blog">Blog</TopLevelNavItem>
-            <TopLevelNavItemNewTab href="https://github.com/lane711/sonicjs">Github</TopLevelNavItemNewTab>
-            <TopLevelNavItemNewTab href="https://discord.gg/8bMy6bv3sZ">Discord</TopLevelNavItemNewTab>
-          </ul>
-        </nav>
-        <div className="hidden md:block md:h-5 md:w-px md:bg-zinc-900/10 md:dark:bg-white/15" />
-        <div className="flex gap-4">
-          <MobileSearch />
-          <ModeToggle />
+        style={{
+          '--bg-opacity-light': bgOpacityLight,
+          '--bg-opacity-dark': bgOpacityDark,
+        }}
+      >
+        <div className="flex items-center gap-5">
+          ⭐  ➝ Help others speed freaks discover SonicJs.
+          <a target="_blank" href="https://github.com/lane711/sonicjs">
+          Star us on GitHub
+          </a>
+          .
         </div>
-        {/* <div className="hidden min-[416px]:contents">
+      </motion.div>
+      <motion.div
+        ref={ref}
+        className={clsx(
+          className,
+          'fixed inset-x-0 z-50 flex h-14 items-center justify-between gap-12 px-4 transition sm:px-6 lg:left-72 lg:z-30 lg:px-8 xl:left-80',
+          !isInsideMobileNavigation &&
+            'backdrop-blur-sm dark:backdrop-blur lg:left-72 xl:left-80',
+          isInsideMobileNavigation
+            ? 'bg-white dark:bg-zinc-900'
+            : 'bg-white/[var(--bg-opacity-light)] dark:bg-zinc-900/[var(--bg-opacity-dark)]'
+        )}
+        style={{
+          '--bg-opacity-light': bgOpacityLight,
+          '--bg-opacity-dark': bgOpacityDark,
+        }}
+      >
+        <div
+          className={clsx(
+            'absolute inset-x-0 top-full h-px transition',
+            (isInsideMobileNavigation || !mobileNavIsOpen) &&
+              'bg-zinc-900/7.5 dark:bg-white/7.5'
+          )}
+        />
+        <Search />
+        <div className="flex items-center gap-5 lg:hidden">
+          <MobileNavigation />
+          <Link href="/" aria-label="Home">
+            <Logo className="h-6" />
+          </Link>
+        </div>
+        <div className="flex items-center gap-5">
+          <nav className="hidden md:block">
+            <ul role="list" className="flex items-center gap-8">
+              <TopLevelNavItem href="/blog">Blog</TopLevelNavItem>
+              <TopLevelNavItemNewTab href="https://github.com/lane711/sonicjs">
+                Github
+              </TopLevelNavItemNewTab>
+              <TopLevelNavItemNewTab href="https://discord.gg/8bMy6bv3sZ">
+                Discord
+              </TopLevelNavItemNewTab>
+            </ul>
+          </nav>
+          <div className="hidden md:block md:h-5 md:w-px md:bg-zinc-900/10 md:dark:bg-white/15" />
+          <div className="flex gap-4">
+            <MobileSearch />
+            <ModeToggle />
+          </div>
+          {/* <div className="hidden min-[416px]:contents">
           <Button href="#">Sign in</Button>
         </div> */}
-      </div>
-    </motion.div>
+        </div>
+      </motion.div>
+    </>
   )
 })
